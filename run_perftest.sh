45defe8e95105d30fc6f0ecf1cf2370d00207468
Joerg Mertin <joerg.mertin@ca.com>
2017-12-20 09:29:52 +0100

    Added append-terse to read-write tests.

--- run_perftest.sh
+++ run_perftest.sh
@@ -7,7 +7,7 @@
 # with the TIM software
 
 # I want it to be verbose.
-VERBOSE=false
+VERBOSE=true
 
 ##########################################################################
 # Nothing to be changed below this point !
@@ -36,7 +36,7 @@ LockFile="${BASEDIR}/${PROGNAME}..LOCK"
 # Define the Hostname
 HOSTName=`hostname -s`
 
-# IP
+# IP / Not working anymore.
 IPAdd=`ifconfig | grep "inet addr:" | head -1 | awk '{ print $2}' | sed -e 's/addr\://g'`
 
 # Logfile - all info will go in there.
@@ -59,6 +59,7 @@ fi
 # Prevent double execution
 Lock $LockFile 1
 
+
 # Create Title
 title "LOG Collector Interactive UI"
 
@@ -67,10 +68,6 @@ LDATE=`date +"%F @ %T"`
 
 # Catch user-name
 USER=`whoami`
-
-# Log program version.
-MSG="$LDATE `whoami`@`hostname` - ${PROGNAME}.sh version $VER"
-title $MSG
 
 
 MSG="Creating logfile $LogFile"
@@ -81,6 +78,10 @@ errors
 # Set a title for the script
 title "Disk performance IOPS test"
 
+# Log program version.
+MSG="$LDATE `whoami`@`hostname` - ${PROGNAME}.sh v$VER (apm-scripts ${RELEASE}-b${BUILD})"
+log $MSG | tee -a $LogFile
+
 if [ "$USER" != "root" ]
 then
 
@@ -89,7 +90,7 @@ then
     errors
 fi
 
-
+# Checking for the FIO binary
 FIO=$( which fio 2>/dev/null )
 
 if ! [ -x "$(command -v ${FIO})" ]
@@ -99,6 +100,7 @@ if ! [ -x "$(command -v ${FIO})" ]
     errors
 fi
 
+# Checking for the IOPing binary
 IOP=$( which ioping 2>/dev/null )
 
 if ! [ -x "$(command -v ${IOP})" ]
@@ -109,7 +111,8 @@ then
 fi
 
 echo "The test directory needs to be situated on the disk/partition"
-echo "that is to be tested. It will be created if non-existent."
+echo "that is to be tested. It will be created if non-existent and"
+echo "its content and itself removed after the test."
 echo -n "Please specific the test-directory [./perftest]: "
 read TESTDIR
 
@@ -123,39 +126,66 @@ then
     errors
 fi
 
+echo
+entry "Running Kernel: uname -a"
+uname -a | tee -a $LogFile
 
+echo
+entry "uptime"
+uptime  | tee -a $LogFile
+
+echo
+entry "OS Information"
+apmsysinfo | tee -a $LogFile
+
+echo
+entry "Memory Usage: free in MBytes"
+free -m | tee -a $LogFile
+echo -n "% Percent used RAM: "  | tee -a  $LogFile
+free | grep Mem: | awk 'FNR == 1 {print $3/($3+$4)*100}'  | tee -a  $LogFile
+
+echo -n "% Percent free RAM: "  | tee -a  $LogFile
+free | grep Mem: | awk 'FNR == 1 {print $4/($3+$4)*100}'  | tee -a  $LogFile
+
+echo
+entry "Processes using SWAP (If empty list - very good !): "
+for file in /proc/*/status ; do awk '/VmSwap|Name/{printf $2 " " $3}END{ print ""}' $file; done | grep -v "0 kB$" | grep " kB$" | sort -k 2 -n -r  | tee -a  $LogFile
+
+echo
+entry "File system disk space usage: df -h"
+df -h  | tee -a  $LogFile
+
+echo
+entry "File system inode usage: df -i"
+df -i  | tee -a  $LogFile
+
+echo
 entry "Random read/write performance"
-echo "Random read/write performance"
-$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=50 | tee -a $LogFile
+$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=randrw --rwmixread=50 --append-terse | tee -a $LogFile
 
-
+echo
 entry "Random read performance"
-echo "Random read performance"
-$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=randread | tee -a $LogFile
+$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=randread --append-terse | tee -a $LogFile
 
-
+echo
 entry "Random write performance"
-echo "Random write performance"
-$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=randwrite | tee -a $LogFile
+$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=randwrite --append-terse | tee -a $LogFile
 
-
+echo
 entry "Sequential read/write performance"
-echo "Sequential read/write performance"
-$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=rw --rwmixread=50 | tee -a $LogFile
+$FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=rw --rwmixread=50 --append-terse | tee -a $LogFile
 
-entry "Sequential read performance"
-echo "Sequential read performance"
+echo
+entry "Sequential read performance" 
 $FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=read | tee -a $LogFile
 
+echo
 entry "Sequential write performance"
-echo "Sequential write performance"
 $FIO --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=fio_test --filename=${DIR}/fio_test.tmp --bs=4k --iodepth=64 --size=4G --readwrite=write | tee -a $LogFile
 
-
-
+echo
 entry "Running IOPing"
-echo "Running IOPing"
-echo "On a healthy system, variation should be very low" | tee -a $LogFile
+log "On a healthy system, variation should be very low" | tee -a $LogFile
 $IOP -c 10 ${DIR}/ | tee -a $LogFile
 
 # Cleaning up
